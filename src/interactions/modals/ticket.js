7141f4f7893ad9d49a1d4ef343bfe5ad2841a701
ofirshasha130-ops <ofirshasha130@gmail.com>
2026-07-09 01:27:01 +0300

    Fix: Add missing renameTicketModalHandler to modal exports
    The renameTicketModalHandler was being imported in the button handlers but was not being exported from the modals/ticket.js file. This caused all interaction handlers to fail when the bot tried to load them.
    
    Added renameTicketModalHandler to the export list.

--- src/interactions/modals/ticket.js
+++ src/interactions/modals/ticket.js
@@ -1,6 +1,7 @@
 import {
   createTicketModalHandler,
   closeTicketModalHandler,
+  renameTicketModalHandler,
 } from '../../handlers/ticketButtons.js';
 
-export default [createTicketModalHandler, closeTicketModalHandler];
+export default [createTicketModalHandler, closeTicketModalHandler, renameTicketModalHandler];
